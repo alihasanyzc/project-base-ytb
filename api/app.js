17894c1659fe37246faa04c4c3e2925d245f3568
alihasanyzc <alihasanyazici28@gmail.com>
2025-08-08 16:24:12 +0300

    veritabanı işlemleri

--- api/app.js
+++ api/app.js
@@ -1,3 +1,5 @@
+if(process.env.NODE_ENV != "production")
+   require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,7 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+console.log("END",process.env);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
